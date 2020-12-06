680e62a1eef66ef567fb2727e7fde5dc8685a3e0
Barak-S <barak.saidoff@gmail.com>
2020-12-06 10:36:07 -0500

    added react hook for trailer in banner.js

--- src/containers/Banner.jsx
+++ src/containers/Banner.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import Requests from '../requests'
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
+
 
 
 const Banner = () => {
 
     const [movie,setMovie] = useState([])
+    const [trailer, setTrailer] = useState('')
 
     useEffect(()=>{
         function fetchData(){
@@ -21,6 +25,35 @@ const Banner = () => {
         return str?.length > n ? str.substr(0,n-1) + "..." : str
     }
 
+    const opts = {
+        height: "390",
+        width: '100%',
+        playerVars: {
+            autplay: 1
+        }
+    }
+
+    const selectPlay=(movie)=>{
+        if (trailer !== "" && !movie){
+            setTrailer('')
+        } else {
+            movieTrailer(movie?.name || movie?.title || movie?.original_name || "")
+            .then(url => {
+                const urlParams = new URLSearchParams(new URL(url).search)
+                if (trailer === urlParams.get('v') ){
+                    setTrailer('')
+                } else {
+                    setTrailer(urlParams.get('v'))
+                }
+            })
+            .catch(err => {
+                console.log(err)
+                setTrailer('')
+            })
+        }
+        console.log(trailer)
+    }
+
     return (
         <header className="banner" style={{
             BackgroundSize: "cover",
@@ -30,12 +63,13 @@ const Banner = () => {
             <div className="banner-contents">
                 <h1 className="banner-title">{ movie?.name || movie?.title || movie?.original_name}</h1>
                 <div className="banner-button">
-                    <button className="banner-button">Play</button>
+                    <button className="banner-button" onClick={()=>selectPlay(movie)}>Play</button>
                     <button className="banner-button">My List</button>
                 </div>
                 <h1 className="banner-description">{concatStr(movie?.overview, 150)}</h1>
             </div>
             <div className="banner-fade-bottom"></div>
+            { trailer !== "" && <YouTube videoId={trailer} opts={opts}/>}
         </header>
     );  
 };
--- src/containers/MoviesContainer.jsx
+++ src/containers/MoviesContainer.jsx
@@ -16,31 +16,37 @@ const MoviesContainer=()=>{
                 title="Trending"
                 key="trending"
                 fetchURL={Requests.fetchTrending}
+                isLargeRow
             />
             <Row
                 title="Action Movies"
                 key="action movies"
                 fetchURL={Requests.fetchAction}
+                isLargeRow
             />
             <Row
                 title="Comedy Movies"
                 key="comedy movies"
                 fetchURL={Requests.fetchComedy}
+                isLargeRow
             />
             <Row
                 title="Horror Movies"
                 key="horror movies"
                 fetchURL={Requests.fetchHorror}
+                isLargeRow
             />
             <Row
                 title="Romance Movies"
                 key="romance movies"
                 fetchURL={Requests.fetchRomance}
+                isLargeRow
             />
             <Row
                 title="Documentaries"
                 key="documentaries"
                 fetchURL={Requests.fetchDocumentaries}
+                isLargeRow
             />
            
         </div>
